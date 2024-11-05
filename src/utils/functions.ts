d4db60287918926ad510ccc0b7c48e5c561e5abc
faridevnz <ale.farinelli@icloud.com>
2024-11-05 10:40:24 +0100

    feat: take database central url from env variable

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,6 +1,12 @@
 import * as dotenv from "dotenv";
+import * as process from "process";
 
 export const loadExternalEnv = async (): Promise<void> => {
   const envPath = ".env";
   dotenv.config({ path: envPath });
+
+  // check required .env variables
+  if (typeof process.env.COSMOPRISM_CENTRAL_DB_URL !== "string") {
+    throw new Error("missing $COSMOPRISM_CENTRAL_DB_URL in your .env file");
+  }
 };
